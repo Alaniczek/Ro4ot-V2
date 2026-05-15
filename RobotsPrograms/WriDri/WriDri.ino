2f2ea1ce06ba5ca60c2c63feb8ec45c1bd17de05
AKU(KU) <AlanPatrykOwczarzak@gmail.com>
2026-05-15 20:32:22 +0200

    Message between PC->ROBOT (one way now)

--- RobotsPrograms/WriDri/WriDri.ino
+++ RobotsPrograms/WriDri/WriDri.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
+const char* WIFI_SSID = "AkuKu";
+const char* WIFI_PASS = "12345678";
+const int LOCAL_PORT  = 4210;
+
+WiFiUDP udp;
+char packetBuffer[255];
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+  
+  udp.begin(LOCAL_PORT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int packetSize = udp.parsePacket();
+  
+  if (packetSize) {
+    memset(packetBuffer, 0, 255);
+    udp.read(packetBuffer, 255);
+    
+    Serial.println(packetBuffer);
+  }
 }
---
+++ core/CommandManager.py
@@ -0,0 +1,39 @@
+﻿import json
+import socket
+
+class CommandManager:
+    def __init__(self, device_ip, device_port, command_list_path):
+        self.device_ip = device_ip
+        self.device_port = int(device_port)
+        self.command_list_path = command_list_path
+        self.command_list = []
+
+    def change_device_settings(self, new_ip, new_port):
+        self.device_ip = new_ip
+        self.device_port = new_port
+    def change_command_list_path(self, new_path):
+        self.command_list_path = new_path
+
+    def load_command_list(self):
+        with open(self.command_list_path, 'r') as file:
+            self.command_list = json.load(file)
+        return self.command_list
+
+    def get_command_list(self):
+        return self.command_list
+
+    def send_command(self, cmd):
+        print(f"snd : {cmd}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(cmd.encode(), (self.device_ip, self.device_port))
+        # sock.sendto((cmd + "\n").encode('utf-8'), (self.device_ip, self.device_port))
+        sock.close()
+
+
+Tester = CommandManager("192.168.243.34", 4210, "komendy.json")
+
+while True:
+    cmd = input("GiveCommand: ")
+    if cmd == 'exit':
+        break
+    Tester.send_command(cmd)
