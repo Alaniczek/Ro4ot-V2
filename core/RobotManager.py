249640980b90d5e0fc5d00bb78b63a29eec7816d
AKU(KU) <AlanPatrykOwczarzak@gmail.com>
2026-05-17 11:07:52 +0200

    Backend without responder is now ready (Pc -> ESP)  (ESP -X-> PC)

--- core/RobotManager.py
+++ core/RobotManager.py
@@ -1,4 +1,7 @@
 ﻿from typing import Optional
+from pathlib import Path
+import json
+
 from RobotKit import RobotKit
 from CommandManager import CommandManager
 
@@ -9,6 +12,25 @@ class RobotManager:
         self.robot_units_path: str = '../../Jsons/robotUnits'
 
     def changePath(self, path: str):
-        self.robot_path = path
-    def selectRobot(self, robot: RobotKit):
+        self.robot_units_path = path
+
+    def selectRobotByKit(self, robot: RobotKit):
         self.selected_robot = robot
+
+    def selectRobotByName(self, robotName: str) -> bool:
+        path_to_robot = Path(self.robot_units_path) / f"{robotName}.json"
+
+        if not path_to_robot.is_file():
+            return False
+
+        data = json.loads(path_to_robot.read_text(encoding="utf-8"))
+
+        robot_kit = RobotKit(
+            data["IP"],
+            int(data["Port"]),
+            data["MODEL"],
+            data["Name"]
+        )
+
+        self.selectRobotByKit(robot_kit)
+        return True
